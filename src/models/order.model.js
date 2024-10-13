c981aaaae971e18ce8fedc32e35d10fa584053b1
nguyenduong1803 <duonghd1803@gmail.com>
2024-10-13 15:25:03 +0700

    feat: create api get order detail by order id

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -7,6 +7,33 @@ class OrderModel extends BaseModel {
       fillable: ["id", "user_id", "total_money", "status", "description"],
     });
   }
+
+  getRoomOrderDetail(orderId) {
+    return new Promise((resolve, reject) => {
+      const queryRoom = `SELECT * FROM cybergame.room_order_detail right join room on room.id = room_order_detail.room_id where room_order_detail.order_id = ${orderId}`;
+      this.connection.query(queryRoom, (error, result) => {
+        if (error) {
+          reject(error);
+        } else if (result && result?.length !== 0) {
+          resolve(result);
+        }
+        resolve();
+      });
+    });
+  }
+  getProductOrderDetail(orderId) {
+    return new Promise((resolve, reject) => {
+      const queryProduct = `SELECT * FROM cybergame.order_detail right join product on product.id = order_detail.product_id where order_detail.order_id = ${orderId}`;
+      this.connection.query(queryProduct, (error, result) => {
+        if (error) {
+          reject(error);
+        } else if (result && result?.length !== 0) {
+          resolve(result);
+        }
+        resolve();
+      });
+    });
+  }
 }
 
 export default new OrderModel();
--- src/route/order/order.route.js
+++ src/route/order/order.route.js
@@ -8,6 +8,7 @@ router.put("/update/:id", orderController.update);
 router.delete("/remove/:id", orderController.deleteById);
 router.get("/searchById/:id", orderController.findById);
 router.get("", orderController.getAll);
+router.get("/detail/:id", orderController.getDetailById);
 
 router.post("/add/payment", paymentController.createPayment);
 router.post("/save/payment", paymentController.savePayment);
