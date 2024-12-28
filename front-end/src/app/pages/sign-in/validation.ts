e00d73ccb9039e3ced13e2be5b10fed7d20eb883
vthuan.dev <165972755+vthuan-dev@users.noreply.github.com>
2024-12-29 00:58:08 +0700

    fix inout sign in

--- front-end/src/app/pages/sign-in/validation.ts
+++ front-end/src/app/pages/sign-in/validation.ts
@@ -1,8 +1,8 @@
 import * as yup from 'yup';
 
 export const SignInSchema = yup.object({
-   username: yup.string().required('Tài khoản không được để trống').max(100, '').default('huatan27'),
-   password: yup.string().required('Mật khẩu không được để trống').max(100, '').default('123456789'),
+   username: yup.string().required('Tài khoản không được để trống').max(100, '').default('admin'),
+   password: yup.string().required('Mật khẩu không được để trống').max(100, '').default('admin123'),
 });
 
 export type SignInPropType = yup.InferType<typeof SignInSchema>;
